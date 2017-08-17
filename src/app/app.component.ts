e1fb99e4b3c9d7a443e43747560a92e2ea5c5cda
Michael Reda <michaelreda96@gmail.com>
2017-08-17 10:51:39 +0200

    added winners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,9 +86,9 @@ export class AppComponent {
     this.http.post('https://landscoringsys.herokuapp.com/backup', { money: money, purchases: purchases, upgrades: upgrades }).subscribe((data) => {
       var res = data['_body'];
       console.log(res);
-      if(res=="OK"){
+      if (res == "OK") {
         alert("backed up");
-      }else{
+      } else {
         alert("error backing up");
       }
     });
@@ -99,23 +99,105 @@ export class AppComponent {
     this.http.get('https://landscoringsys.herokuapp.com/restore_money').subscribe((data) => {
       var money = JSON.parse(data['_body']);
       console.log(money);
-      localStorage.setItem('money',money.backup);
+      localStorage.setItem('money', money.backup);
       this.http.get('https://landscoringsys.herokuapp.com/restore_purchases').subscribe((data) => {
         var purchases = JSON.parse(data['_body']);
         console.log(purchases);
-        localStorage.setItem('purchases',purchases.backup);
+        localStorage.setItem('purchases', purchases.backup);
         this.http.get('https://landscoringsys.herokuapp.com/restore_upgrades').subscribe((data) => {
           console.log(data)
           var upgrades = JSON.parse(data['_body']);
           console.log(upgrades);
-          localStorage.setItem('upgrades',upgrades.backup);
+          localStorage.setItem('upgrades', upgrades.backup);
           this.reload_page();
         });
       });
     });
-
-
   }
+
+  get_winners() {
+    var password = prompt("WINNERS PASSWORD ?");
+    if (password != "HOBA") {
+      return;
+    }
+    var teams_wealth = [];
+    var teams_money = JSON.parse(localStorage.getItem("money"));
+    var teams_purchases = JSON.parse(localStorage.getItem("purchases"));
+    var teams_upgrades = JSON.parse(localStorage.getItem("upgrades"));
+    for (let i = 1; i < 5; i++) {
+      let team_wealth = 0;
+      team_wealth += this.calculate_money(i,teams_money['team'+i]);
+      team_wealth += this.spent_money(i);
+      console.log(team_wealth);
+      // AppSettings['team'+i+'_name']
+      teams_wealth.push(JSON.stringify({'team_name': AppSettings['team'+i+'_name'],'team_wealth':team_wealth}));
+    }
+    teams_wealth=teams_wealth.sort((a,b)=>{
+      var aa= JSON.parse(a);
+      var bb= JSON.parse(b);
+      return bb.team_wealth-aa.team_wealth;
+    });
+    var results="Rankings: \n";
+    for(let i=0;i<teams_wealth.length;i++){
+      let rank = i+1;
+      results+= ""+rank+") "+JSON.parse(teams_wealth[i]).team_name+" : "+JSON.parse(teams_wealth[i]).team_wealth+"\n";
+    }
+    alert(results);
+  }
+
+  public calculate_money(team, team_money) {
+    var money = 0;
+    money += team_money;//bank
+    money += this.total_profit(JSON.parse(localStorage.getItem('purchases'))['team'+team],JSON.parse(localStorage.getItem('upgrades'))['team'+team]);
+    // money -= this.spent_money();
+    return money;
+  }
+
+  total_profit(team_purchases,team_upgrades) {
+    var sum = 0;
+    if (team_purchases.hotel.purchased) {
+      sum += this.calculate_hours(team_purchases.hotel.purchased_since) * team_purchases.hotel.rooms * AppSettings.room_per_hour;
+      for (let i = 0; i < team_upgrades.rooms.length; i++) {
+        sum += this.calculate_hours(team_upgrades.rooms[i]) * 10 * AppSettings.room_per_hour;
+      }
+    }
+    return sum;
+  }
+
+  calculate_hours(x) {
+    var input_date: any = new Date(x);
+    var now: any = new Date(Date.now());
+    return Math.floor(Math.abs(now - input_date) / 36e5);
+  }
+
+   public spent_money(team) {
+    var sum = 0;
+    var team_purchases=JSON.parse(localStorage.getItem('purchases'))['team'+team];
+    var team_upgrades=JSON.parse(localStorage.getItem('upgrades'))['team'+team];
+    if (team_purchases.hospital.purchased) {
+      sum += AppSettings.HOSPITAL_PRICE;
+      sum += team_upgrades.beds.length * AppSettings.ten_beds_price;
+    }
+
+    if (team_purchases.hotel.purchased) {
+      sum += AppSettings.HOTEL_PRICE;
+      sum += team_upgrades.rooms.length * AppSettings.ten_rooms_price;
+    }
+
+    if (team_purchases.restaurant.purchased) {
+      sum += AppSettings.RESTAURANT_PRICE;
+      sum += team_upgrades.tables.length * AppSettings.ten_tables_price;
+    }
+
+    if (team_purchases.cars_shop.purchased) {
+      sum += AppSettings.CARS_SHOP_PRICE;
+      sum += team_upgrades.cars.length * AppSettings.ten_cars_price;
+    }
+
+    // console.log("spent money : " + sum);
+    return sum;
+  }
+
 }
 export interface Team {
   money: number;
